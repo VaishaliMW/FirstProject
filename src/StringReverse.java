e7583bc1c214faf74dee647aa0fccaacf8fb9a4f
Sam SmithVaishali <vaishaliwadaskar12@gamil.com>
2023-07-31 14:06:10 +0530

    This commite is made by us time zone guy

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -12,6 +12,8 @@ public class StringReverse {
 	String s="Vaishali";
 	 char a[]= s.toCharArray();
 	 System.out.println("Reverse String is : ");
+	 System.out.println("1. First Changes");
+	 System.out.println("2.second change ");
 	 for(int i=a.length-1;i>=0;i-- ) {
 		 
 		 System.out.print(a[i]);
